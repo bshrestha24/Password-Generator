130cf404964e179ea29794af39cb66be5df2c338
bshrestha24 <bikasshrestha0413@gmail.com>
2025-06-04 17:54:06 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pw';
+
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+
+ onChangeLength(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const parsedValue = parseInt(input.value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    } else {
+    this.length = 0; // Set to 0 if input is empty or invalid
+  }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+
+  onButtonClick(){
+    
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbol = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if(this.includeLetters){
+      validChars += letters;
+    }
+
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+
+    if(this.includeSymbols){
+      validChars += symbol;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+  }
+
 }
